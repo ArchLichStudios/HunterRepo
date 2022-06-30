2c9a0041ff7afce1a8d31b04a136deff0a5bc710
Tony Morgan <ArchlichStudios@gmail.com>
2022-06-30 15:43:26 -0400

    Initialize
    Initialize Repository

---
+++ TheHunter/Source/TheHunter/TheHunterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheHunterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEHUNTER_API ATheHunterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
